3737de148cb32841280530a10640d0011c20ccfe
Friederike Schueuer <Friederike.schueuer@gmail.com>
2014-06-24 11:57:20 -0400

    fix style issues

--- pythonFiles/determineStartOfRecording.py
+++ pythonFiles/determineStartOfRecording.py
@@ -18,11 +18,11 @@ import re
 files = [f for f in os.listdir(dataPath) if os.path.isfile(f) and 'RePORTER' in f]
 startDates = {}
 for file in files:
-	d = pd.read_csv(file)
-	headers = d.columns.values.tolist()
+	data = pd.read_csv(file)
+	headers = data.columns.values.tolist()
 	year = re.search(r'\d\d\d\d', file).group()	
 	for header in headers:
-		entries = d[header].dropna()
+		entries = data[header].dropna()
 		if not entries.any():
 			print "No entries for " + header + " in " + year + "\n"
 		elif not header in startDates:
